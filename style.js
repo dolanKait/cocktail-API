84d6811bae877c1f2916e2a56e1942e7ac7c8bb3
Kaitlyn Dolan <dolankaitlyn17@gmail.com>
2022-04-06 22:57:46 -0500

    4/6

--- style.js
+++ style.js
@@ -1,18 +1,18 @@
-
-
-document.getElementsByClassName('vodka').addEventListener('click', getVodka())
+document.getElementById('vodka').addEventListener('click', getVodka)
 
 function getVodka(){
-    
-    const url = "www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka"
-    
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka"
+
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            document.querySelector('img').src=data
+            console.log(data.drinks[0])
+            document.querySelector('img').src=data.drinks[0].strDrinkThumb
+            document.querySelector('h2').innerText=data.drinks[0].strDrink
+            document.querySelector('section').innerText=data.drinks[0].strInstructions
+
         })
-    } 
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+        .catch(err => {
+            console.log(`error ${err}`)
+         });
+}
